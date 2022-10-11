46360e45d539dc58b2439e71a6683314d6ecb655
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2022-10-11 09:07:38 -0700

    added function to validate input

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
-    alert("Reg button was clicked");
+    inputValid("first-name", "First name is required");
+    inputValid("last-name", "Last name is required");
+}
+function inputValid(id, errMsg) {
+    var txtBox = document.getElementById(id);
+    var txtBoxValue = txtBox.value;
+    if (txtBoxValue == "") {
+        var errSpan = txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -4,5 +4,24 @@ window.onload = function(){
 }
 
 function main():void{
-    alert("Reg button was clicked");
+    inputValid("first-name", "First name is required");
+    inputValid("last-name","Last name is required");
+}
+
+/**
+ * 
+ * @param id id of input text box
+ * @param errMsg errMsg to display in sibling span of input text box if false
+ * @returns true if string isn't empty
+ */
+function inputValid(id:string, errMsg:string):boolean {
+    let txtBox = <HTMLInputElement>document.getElementById(id);
+    let txtBoxValue = txtBox.value;
+
+    if (txtBoxValue == "") {
+        let errSpan = <HTMLSpanElement>txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
